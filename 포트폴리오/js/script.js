c46f4c6f02f3e894b52a36892795eb71a5a106e3
SoHyeon <thguskim12@gmail.com>
2024-11-29 18:06:19 +0900

    1129

--- 포트폴리오/js/script.js
+++ 포트폴리오/js/script.js
@@ -1,5 +1,44 @@
 
-const $notie = document.querySelector(".notie");
+import ("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+import ("https://cdnjs.cloudflare.com/ajax/libs/jquery-circle-progress/1.2.2/circle-progress.min.js");
+
+
+$(document).ready(function () {
+  $('.circle-graph').each(function () {
+    // 각각의 circle-graph 요소를 가져옵니다.
+    const $this = $(this);
+
+    // HTML 속성에서 circleProgress 값과 색상 값을 읽어옵니다.
+    const perNum = parseFloat($this.attr('circleProgress')) || 0;
+    const colors = JSON.parse($this.attr('data-colors')) || ["#e5ecff", "#ffadc7"];
+
+    // circleProgress를 초기화합니다.
+    $this.circleProgress({
+      size: 100, // 그래프 크기
+      value: perNum / 100, // 그래프에 표시될 값
+      startAngle: 300, // 시작지점
+      thickness: 5, // 그래프 두께
+      fill: { // 그래프 선 색상
+        gradient: colors,
+        gradientAngle: Math.PI / 4
+      },
+      animation: {
+        duration: 2200,
+        easing: "swing"
+      },
+      lineCap: "butt", // 그래프 선 모양
+      reverse: false // 그래프가 진행되는 방향
+    }).on('circle-animation-progress', function (event, progress) {
+      // 진행 중인 상태 업데이트
+      $this.find('.circle-percent').html(Math.round(perNum * progress) + '<i>%</i>');
+    });
+  });
+});
+
+
+
+
+  const $notie = document.querySelector(".notie");
 
 // 브라우저 창 크기에 따른 별 생성
 window.onresize = () => {
@@ -39,6 +78,7 @@ function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
 //meteoros
 
 var numeroAleatorio = 5000;
@@ -56,4 +96,31 @@ function carregarMeteoro(){
     document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
   }, 1000);
 }
-  window.onload = init;
+
+
+document.getElementById('toggleTheme').addEventListener('click', () => {
+  const waveForms = document.getElementById('wave-forms');
+  const backSky = document.getElementById('backSky');
+  const formElement = document.querySelector('.form');
+  const body = document.body;
+  const HD = document.querySelector('.HD')
+  const Ul = document.querySelector('#header')
+
+  // 현재 display 상태를 확인하고 토글
+  if (waveForms.style.display === 'block') {
+    waveForms.style.display = 'none';
+    backSky.style.display = 'block';
+    formElement.style.backgroundImage = 'url("img/gradient-blue-abstract-background-smooth-dark-blue-with-black-vignette-studio.jpg")'
+    body.classList.add('dark-mode')
+    Ul.classList.add('dark')
+    
+    
+  } else {
+    waveForms.style.display = 'block';
+    backSky.style.display = 'none';
+    formElement.style.backgroundImage = 'url("img/blurred-abstract-background.jpg")'
+    body.classList.remove('dark-mode');
+    Ul.classList.remove('dark')
+
+  }
+});
