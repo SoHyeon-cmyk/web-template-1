065603b27905b7d579af62821f50adcf0eef8cb5
SoHyeon <thguskim12@gmail.com>
2024-12-04 17:56:34 +0900

    1204

--- 포트폴리오/js/script.js
+++ 포트폴리오/js/script.js
@@ -105,6 +105,8 @@ document.getElementById('toggleTheme').addEventListener('click', () => {
   const body = document.body;
   const HD = document.querySelector('.HD')
   const Ul = document.querySelector('#header')
+  // 애니메이션 대상 선택
+  const bannerElements = document.querySelectorAll('.banner img, .banner h2, .banner h4');
 
   // 현재 display 상태를 확인하고 토글
   if (waveForms.style.display === 'block') {
@@ -124,4 +126,15 @@ document.getElementById('toggleTheme').addEventListener('click', () => {
 
   }
 });
+
+
+ 
+  // 애니메이션 재실행
+  bannerElements.forEach(el => {
+    el.classList.remove('animate'); // 클래스 제거
+    void el.offsetWidth; // 리플로우 강제 적용 (DOM 재계산)
+    el.classList.add('animate'); // 클래스 다시 추가
+  });
+
+
 
