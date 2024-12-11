0443551a41d17dbd20712fde8bc0222a19f2e117
SoHyeon <thguskim12@gmail.com>
2024-12-11 18:06:51 +0900

    1211

--- 포트폴리오/js/script.js
+++ 포트폴리오/js/script.js
@@ -106,10 +106,10 @@ document.getElementById('toggleTheme').addEventListener('click', () => {
   const HD = document.querySelector('.HD')
   const Ul = document.querySelector('#header')
   let copyModal =document.getElementById('copied')
-  // 애니메이션 대상 선택
+
   const bannerElements = document.querySelectorAll('.banner img, .banner h2, .banner h4');
 
-  // 현재 display 상태를 확인하고 토글
+ 
   if (waveForms.style.display === 'block') {
     waveForms.style.display = 'none';
     backSky.style.display = 'block';
@@ -132,11 +132,11 @@ document.getElementById('toggleTheme').addEventListener('click', () => {
 
 
  
-  // // 애니메이션 재실행
+
   // bannerElements.forEach(el => {
-  //   el.classList.remove('animate'); // 클래스 제거
-  //   void el.offsetWidth; // 리플로우 강제 적용 (DOM 재계산)
-  //   el.classList.add('animate'); // 클래스 다시 추가
+  //   el.classList.remove('animate');
+  //   void el.offsetWidth; 
+  //   el.classList.add('animate'); 
   // });
 
   
